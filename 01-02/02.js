16ef4184879a5e8afb7d379f4c946a9a64216910
Patricia Grings <patriciagrings@hotmail.com>
2024-10-30 16:59:23 -0300

    atualizacao

---
+++ 01-02/02.js
@@ -0,0 +1,13 @@
+var animais = {
+    animal: cachorro, gato, 
+    emitir_som: function(){
+        if (animal == cachorro) {
+            console.log('auau')
+        }
+        else {
+            console.log('miau')
+        }
+    }
+}
+
+animais.emitir_som()
